e58814aeb29ae9b1ed6e3457954084f9feb3febd
SH44N96 <sh44n96@users.noreply.github.com>
2020-07-16 13:22:17 +0800

    Player Movement Horizontal

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        if(controller == null)
+        {
+            Debug.LogError("Player: CharacterController is NULL");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        controller.Move(Vector3.right * horizontalInput * Time.deltaTime);
+    }
+}
