243948136df2fbe77ba3e607265f2af4bd635aac
SH44N96 <sh44n96@users.noreply.github.com>
2020-07-16 15:44:11 +0800

    Moving Platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Transform targetA, targetB;
+    [SerializeField] private float speed = 5;
+    private bool switching;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position == targetA.position)
+        {
+            switching = false;
+        }
+        else if(transform.position == targetB.position)
+        {
+            switching = true;
+        }
+
+        if(switching)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetA.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetB.position, speed * Time.deltaTime);
+        }
+    }
+}
