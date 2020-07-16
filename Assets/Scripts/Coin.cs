2abb56c6719cb53af1aa52e603b38b63f2e20426
SH44N96 <sh44n96@users.noreply.github.com>
2020-07-16 15:14:39 +0800

    Coin Collection & Display

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        if(player == null)
+        {
+            Debug.LogError("Coin: Player is NULL");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            player.AddCoins();
+
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,21 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 5;
     [SerializeField] private float gravity = 1;
     [SerializeField] private float jumpHeight = 20;
+    private UIManager uiManager;
     private CharacterController controller;
     private float yVelocity;
+    private int coins;
     private bool canDoubleJump;
 
     // Start is called before the first frame update
     void Start()
     {
+        uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        if(uiManager == null)
+        {
+            Debug.LogError("Player: UIManager is NULL");
+        }
+
         controller = GetComponent<CharacterController>();
         if(controller == null)
         {
@@ -53,5 +61,12 @@ public class Player : MonoBehaviour
         velocity.y = yVelocity;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void AddCoins()
+    {
+        coins++;
+
+        uiManager.UpdateCoinsDisplay(coins);
     }
 }
